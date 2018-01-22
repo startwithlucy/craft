2102dc48ce9a3e3852c278d2dd1302457afbc6ee
Matt Blake <myblake@gmail.com>
2018-01-22 15:55:35 -0800

    added procifle and whatnot

--- config/db.php
+++ config/db.php
@@ -6,13 +6,13 @@
  * list of the available settings in vendor/craftcms/cms/src/config/DbConfig.php.
  */
 
-return [
-    'driver' => getenv('DB_DRIVER'),
-    'server' => getenv('DB_SERVER'),
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_DATABASE'),
-    'schema' => getenv('DB_SCHEMA'),
-    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
-];
+$url = getenv('DATABASE_URL');
+$dbparts = parse_url($url);
+
+return array(
+  'server' => $dbparts['host'],
+  'user' => $dbparts['user'],
+  'password' => $dbparts['pass'],
+  'database' => ltrim($dbparts['path'],'/'),
+  'tablePrefix' => 'craft',
+);
